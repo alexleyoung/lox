b417b55cb88a4d4e7aff9f26ce03d05bf80ccdb4
Alex Young <alexyoung0119@gmail.com>
2025-11-29 22:55:30 -0600

    interpreter prog

--- golox/interpreter.go
+++ golox/interpreter.go
@@ -22,23 +22,6 @@ func (i *Interpreter) VisitUnaryExpr(expr UnaryExpr) (any, error) {
 	return nil, nil
 }
 
-func (i *Interpreter) isTruthy(v any) bool {
-	switch v.(type) {
-	case bool:
-		return v.(bool)
-	case int:
-		return v.(int) != 0
-	case float64:
-		return v.(float64) != 0
-	case string:
-		return v.(string) != ""
-	case nil:
-		return false
-	default:
-		return true
-	}
-}
-
 func (i *Interpreter) VisitGroupingExpr(expr GroupingExpr) (any, error) {
 	return i.evaluate(expr.Expr)
 }
@@ -48,5 +31,83 @@ func (i *Interpreter) evaluate(expr Expr) (any, error) {
 }
 
 func (i *Interpreter) VisitBinaryExpr(expr BinaryExpr) (any, error) {
+	left, err := i.evaluate(expr.Left)
+	if err != nil {
+		return nil, err
+	}
+	right, err := i.evaluate(expr.Right)
+	if err != nil {
+		return nil, err
+	}
 
+	switch expr.Op.Type {
+	case SLASH:
+		return left.(float64) / right.(float64), nil
+	case MINUS:
+		return left.(float64) - right.(float64), nil
+	case STAR:
+		return left.(float64) * right.(float64), nil
+	// use + for string concat and number addition
+	case PLUS:
+		switch left.(type) {
+		case string:
+			return left.(string) + right.(string), nil
+		case float64:
+			return left.(float64) + right.(float64), nil
+		}
+
+	// only supported between numbers
+	case LESS:
+		return left.(float64) < right.(float64), nil
+	case LESS_EQUAL:
+		return left.(float64) <= right.(float64), nil
+	case GREATER:
+		return left.(float64) > right.(float64), nil
+	case GREATER_EQUAL:
+		return left.(float64) >= right.(float64), nil
+
+	case EQUAL_EQUAL:
+		return left == right, nil
+	case BANG_EQUAL:
+		return !(left == right), nil
+	}
+
+	return nil, nil
+}
+
+func (i *Interpreter) VisitTernaryExpr(expr TernaryExpr) (any, error) {
+	condition, err := i.evaluate(expr.Guard)
+	if err != nil {
+		return nil, err
+	}
+	thenBranch, err := i.evaluate(expr.Then)
+	if err != nil {
+		return nil, err
+	}
+	elseBranch, err := i.evaluate(expr.Else)
+	if err != nil {
+		return nil, err
+	}
+
+	if i.isTruthy(condition) {
+		return thenBranch, nil
+	}
+	return elseBranch, nil
+}
+
+func (i *Interpreter) isTruthy(v any) bool {
+	switch v := v.(type) {
+	case bool:
+		return v
+	case int:
+		return v != 0
+	case float64:
+		return v != 0
+	case string:
+		return v != ""
+	case nil:
+		return false
+	default:
+		return true
+	}
 }
