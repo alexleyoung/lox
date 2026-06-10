012ab44b9afdbb497fb76ca26b3aec9ee103cd41
Alex Young <alexyoung0119@gmail.com>
2026-06-09 23:54:20 -0400

    debug disassembler

--- clox/main.c
+++ clox/main.c
@@ -1,8 +1,10 @@
 #include "chunk.h"
+#include "debug.h"
 
 int main() {
   Chunk chunk;
   init_chunk(&chunk);
   write_chunk(&chunk, OP_RETURN);
+  disassemble_chunk(&chunk, "test");
   free_chunk(&chunk);
 }
